737bf34222578e1cd07b1fa895349ba3b0a02579
Ken Ling <ken88ling@gmail.com>
2016-09-19 14:33:27 +0800

    add javascript

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// site.js
+
+(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Shawn Wildermuth";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style = "background: #888;";
+    };
+
+    main.onmouseleave = function() {
+        main.style = "";
+    };
+})();
