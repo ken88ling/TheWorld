d076cb5e8ab0d2d6ba2b8931a997c4cd3127ad6f
Ken Ling <ken88ling@gmail.com>
2016-09-20 12:25:34 +0800

    Enable Mvc

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -16,6 +16,7 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,9 @@ namespace TheWorld
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles(); // we don't want the wwwroot's html
+
+            app.UseMvc();
             app.UseStaticFiles();
 
         }
