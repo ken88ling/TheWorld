cf7ed52c7b43063e35c4b2abc014c33be4b85668
Ken Ling <ken88ling@gmail.com>
2016-09-21 14:30:29 +0800

    add mail service
    add mail services
    dependency injection
    add the config file

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService ;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService,IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
         public IActionResult Index()
         {
 
@@ -18,6 +29,15 @@ namespace TheWorld.Controllers.Web
             return View();
         }
 
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            
+            _mailService.SendMail(_config["MailSettings:ToAddress"],model.Email, "from theworld" , model.Message);
+
+            return View(model);
+        }
         public IActionResult About()
         {
             return View();
