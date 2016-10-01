5182cd737afb39eeb0420ecdfddeae72eb4d8f38
ken88ling <ken88ling@gmail.com>
2016-10-01 15:37:13 +1000

    EF Core - Using the DbContext
    Create a world context.cs
    Add services.AddDbContext in startup class inside ConfigureServices
    method

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Models;
 using TheWorld.Services;
 
 namespace TheWorld
@@ -35,7 +36,9 @@ namespace TheWorld
             else
             {
                 //implement a real mail service
-            }   
+            }
+
+            services.AddDbContext<WorldContext>();
             services.AddMvc();
         }
 
