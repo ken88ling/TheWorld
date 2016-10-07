8662528706a3660980c3d04978f1ba43e8cffd51
ken88ling <ken88ling@gmail.com>
2016-10-08 00:38:34 +1100

    Angular - Retrieving Data and Load from API
    loading from api using $http.get("api/trips")

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -6,19 +6,25 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
         
-        vm.trips = [{
-                name: "US Trip",
-                created:new Date()
-            }, {
-                name: "World Trips",
-                created:new Date()
-            }];
+        // retrieve from api - using http
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+
+        $http.get("/api/trips")
+            .then(function(response) {
+                //Success
+                angular.copy(response.data, vm.trips);
+            }),function() {
+                // failure
+            vm.errorMessage = "Failed to load data: " + error;
+        }
 
         vm.addTrip = function() {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
