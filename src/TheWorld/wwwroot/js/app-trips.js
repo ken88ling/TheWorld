be359e29086fa185d460e31a0f86716d3d0067bf
ken88ling <ken88ling@gmail.com>
2016-10-08 16:08:02 +1100

    Angular-Route - Adding Routing

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -1,12 +1,20 @@
-﻿(function() {
+﻿(function () {
 
     "use strict";
 
 
     //Create the module
     angular.module("app-trips", ["simpleControls", "ngRoute"])
-    .config(function() {
-            
+    .config(function ($routeProvider) {
+
+        $routeProvider.when("/",
+        {
+            controller: "tripsController",//this it's going to be used for that individual view
+            controllerAs: "vm", //for data binding
+            templateUrl: "/views/tripsView.html"//represent what is actual view
         });
+
+        $routeProvider.otherwise({ redirectTo: "/" });
+    });
 
 })();
