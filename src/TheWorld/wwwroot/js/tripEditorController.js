bec94e44a173c7bb60263ad95e5ecdae60cd70c1
ken88ling <ken88ling@gmail.com>
2016-10-09 01:50:59 +1100

    Angular-Route - Creating a New Stop
    using http post to create a new stop
    and update the map

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -13,7 +13,8 @@
         vm.isBusy = true;
         vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+        $http.get(url)
             .then(function (response) {
                 //success
                 angular.copy(response.data, vm.stops);
@@ -26,12 +27,33 @@
         .finally(function () {
             vm.isBusy = false;
         });
+
+        vm.addStop = function () {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    //success
+                    vm.stops.push(response.data);
+                    showMap(vm.stops);
+                    vm.newStop = {};
+                },
+                function (err) {
+                    //failure
+                    vm.errorMessage = "Failed to add new stop";
+                })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+        };
     }
 
+    //2nd function
     function showMap(stops) {
         if (stops && stops.length > 0) {
 
-            var mapStops = _.map(stops,function(item) {
+            var mapStops = _.map(stops, function (item) {
                 return {
                     lat: item.latitude,
                     long: item.longitude,
